9ffeea63f5419bd51ef218f8bf177809e060d2d2
VikaUrzan <vikysya163945@gmail.com>
2023-11-12 23:14:29 +0200

    Task 1 "You must add a sing method to the Bird class that prints in console "I am singing", then overload the method accordingly so that the code prints the following lines:"

---
+++ src/Lesson4/Main.java
@@ -0,0 +1,11 @@
+package Lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        bird.action(10);
+        bird.action(1.7);
+        bird.action("Vika");
+        bird.action();
+    }
+}
