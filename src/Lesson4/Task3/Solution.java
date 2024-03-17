06fe06f0114b7c623ac9f0efb971eaab742784e4
VikaUrzan <vikysya163945@gmail.com>
2024-03-17 09:24:16 +0100

    - Changed Solution class (name of objects)

--- src/Lesson4/Task3/Solution.java
+++ src/Lesson4/Task3/Solution.java
@@ -9,15 +9,15 @@ public class Solution {
         myCourseJava.setHoursDuration(12);
         Course newCoursePython = new Course(new Date(), "Python");
         Course newCourseSwift = new Course(15, "Swift", "Bogdan");
-        Student studentAlex = new Student("First", "Alex", 15);
-        Student FirstCourses = new Student("First", new Course[1]);
-        CollegeStudent collegeStudentRatingDima = new CollegeStudent("Dima", 10, 123456789);
-        CollegeStudent collegeStudentNameUrzan = new CollegeStudent("Urzan", "Vika", 15);
+        Student studentAlexFirst = new Student("Alex", "First", 15);
+        Student coursesAlexFirst = new Student("First", new Course[1]);
+        CollegeStudent collegeStudentRatingID123456789 = new CollegeStudent("Dima", 10, 123456789);
+        CollegeStudent collegeStudentNameVikaUrzan = new CollegeStudent("Vika", "Urzan", 15);
         CollegeStudent collegeStudentCoursesSecond = new CollegeStudent("Second", new Course[2]);
         SpecialStudent specialStudentKey15 = new SpecialStudent(15);
-        SpecialStudent specialStudentRatingOleg = new SpecialStudent("Oleg", 25, 123445678);
-        SpecialStudent specialStudentNameOleg = new SpecialStudent("Oleg", "Third", 15);
+        SpecialStudent specialStudentRatingID123445678 = new SpecialStudent("Oleg", 25, 123445678);
+        SpecialStudent specialStudentNameOlegThird = new SpecialStudent("Oleg", "Third", 15);
         SpecialStudent specialStudentKey456 = new SpecialStudent(456);
-        SpecialStudent specialStudentGroup1Olya = new SpecialStudent("Olya", "Next", 1);
+        SpecialStudent specialStudentOlyaNext = new SpecialStudent("Olya", "Next", 1);
     }
 }
